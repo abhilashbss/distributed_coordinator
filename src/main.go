18ade7eef78b49171a2624e48af36bc5caa4629f
abhilashbss <abhilashbss@gmail.com>
2021-11-09 09:02:25 -0800

    CLI args for running coordinator

--- src/main.go
+++ src/main.go
@@ -1,64 +1,34 @@
 package main
 
 import (
-	"sync"
-	"time"
+	"flag"
+	"fmt"
 
 	logger "github.com/abhilashbss/distributed_coordinator/src/Logger"
 	coord "github.com/abhilashbss/distributed_coordinator/src/coordinator"
 )
 
 func main() {
-	logger.InitLogger("/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/log/logs.txt")
+	arg_map := parseArgs()
+	fmt.Println(arg_map)
+	logger.InitLogger(arg_map["log_path"])
 	coord1 := coord.CoordActor{}
-	coord1.Node_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/node_init.conf"
-	coord1.Cluster_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/cluster_meta.conf"
+	coord1.Node_conf_path = arg_map["node_conf_path"]
+	coord1.Cluster_conf_path = arg_map["cluster_meta_path"]
 	coord1.LoadCoordinator()
 	coord1.Listen()
 }
 
-func FourNodeSeedNodeJoinTest() {
+func parseArgs() map[string]string {
+	arg_map := make(map[string]string)
 
-	var waitGroup sync.WaitGroup
-	waitGroup.Add(4)
-	// Seed node is run first
-	logger.InitLogger("/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/log/logs.txt")
-	coord1 := coord.CoordActor{}
-	coord1.Node_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/node_init_1.conf"
-	coord1.Cluster_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/cluster_meta.conf"
-	coord1.LoadCoordinator()
-	go func() {
-		coord1.Listen()
-	}()
-	time.Sleep(1 * time.Second)
-
-	// All other nodes
-	coord2 := coord.CoordActor{}
-	coord2.Node_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/node_init_2.conf"
-	coord2.Cluster_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/cluster_meta.conf"
-	go func() {
-		coord2.Listen()
-	}()
-	time.Sleep(1 * time.Second)
-	coord2.LoadCoordinator()
-
-	coord3 := coord.CoordActor{}
-	coord3.Node_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/node_init_3.conf"
-	coord3.Cluster_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/cluster_meta.conf"
-	go func() {
-		coord3.Listen()
-	}()
-	time.Sleep(1 * time.Second)
-	coord3.LoadCoordinator()
-
-	coord4 := coord.CoordActor{}
-	coord4.Node_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/node_init_4.conf"
-	coord4.Cluster_conf_path = "/home/abhilashbss/go/src/github.com/abhilashbss/distributed_coordinator/conf/cluster_meta.conf"
-	go func() {
-		coord4.Listen()
-	}()
-	time.Sleep(1 * time.Second)
-	coord4.LoadCoordinator()
-
-	waitGroup.Wait()
+	var log_path, node_init_path, cluster_meta_path string
+	flag.StringVar(&log_path, "log_path", "/log", "log file path")
+	flag.StringVar(&node_init_path, "node_conf_path", "/node_init.conf", "init conf file path")
+	flag.StringVar(&cluster_meta_path, "cluster_conf_path", "/cluster_meta.conf", "cluster meta conf file path")
+	flag.Parse()
+	arg_map["log_path"] = log_path
+	arg_map["node_conf_path"] = node_init_path
+	arg_map["cluster_meta_path"] = cluster_meta_path
+	return arg_map
 }
