a35c25c0abc48326487340974ee01d5f436a6ffa
abhilashbss <abhilashbss@gmail.com>
2021-11-19 23:01:09 -0800

    Code cleaning

--- src/coordinator/ServiceMessageProcessor.go
+++
@@ -1,9 +0,0 @@
-package coordinator
-
-import messaging "github.com/abhilashbss/distributed_coordinator/src/messaging"
-
-type ServiceMessageProcessor struct {
-}
-
-func (c *ServiceMessageProcessor) ExecuteActionForMessage(m messaging.Message) {
-}
